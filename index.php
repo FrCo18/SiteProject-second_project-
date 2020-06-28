ce1da2f3ff06c43c357689d839b2d25845ec5d9d
FrCo18 <56839873+FrCo18@users.noreply.github.com>
2020-06-28 08:33:51 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+session_start();
+require_once "vihod.php";
+    ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>SiteProject</title>
+</head>
+<body>
+    <?php require_once "files/header.php";?>
+
+    <?php require_once "files/footer.php";?>
+</body>
+</html>
